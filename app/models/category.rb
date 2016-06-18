f35f36f1c16e37054b8bc341b6a2a78d6b3c2f3d
Le Hoang Vu <lehoangvu1992@gmail.com>
2016-06-18 12:55:17 +0700

    Fix category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,3 @@
-class CreateCategories < ActiveRecord::Migration
-  def change
-    create_table :categories do |t|
-      t.string :name
-
-      t.timestamps null: false
-    end
-  end
+class Category < ActiveRecord::Base
+	has_many :products
 end
